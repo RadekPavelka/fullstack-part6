1a78e3edc89dc632ceaec1c5f3eb9aaf3e5f275b
Radek Pavelka <radekpavelka94@gmail.com>
2022-03-08 15:24:34 +0100

    complete exercises 6.13 and 6.14

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -2,8 +2,17 @@ import NewAnecdote from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { useEffect } from 'react'
+import anecdoteService from './services/anecdotes'
+import { useDispatch } from 'react-redux'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
 
   return (
     <div>
--- redux-anecdotes/src/store.js
+++ redux-anecdotes/src/store.js
@@ -12,4 +12,6 @@ const store = configureStore({
 })
 console.log(store.getState())
 
+
+
 export default store
